b19fc222cc992c77baaf63be705095e259f894c9
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2019-04-03 00:37:49 -0600

    adding changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
+// Load User Model
+const User = require('../../models/User');
 
 // @route GET api/users/test
 // @desc Test users route
@@ -10,5 +11,26 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg:"users works"
 }));
+// @route GET api/users/test
+// @desc Regisger a user
+// @access Public
+
+router.post('/register', (req, res)=>{
+ User.findOne({email: req.body.email})
+ .then(user => {
+     if(user){
+         return res.status(400).json({email: 'Email already exist'});
+     } else {
+         const newUser = new User({
+             name: req.body.name,
+             email: req.body.email,
+             avatar,
+             password: req.body.password
+
+         });
+     }
+ })
+});
+
 
 module.exports = router;
