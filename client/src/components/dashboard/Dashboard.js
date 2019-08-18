27b970dd4f5bf8a2238fd7a041ec0fad3980f989
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2019-08-18 11:04:11 -0600

    adding Private Route and some fixes

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profileActions';
@@ -9,14 +10,13 @@ class Dashboard extends Component {
       this.props.getCurrentProfile();
   }
   render() {
-  
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
   
     let dashboardContent;
   
     if( profile === null || loading ){
-      dashboardContent = <Spinner />
+      dashboardContent = <Spinner />;
     } else{
       // Check if the logged user has profile data
       if(Object.keys(profile).length > 0 ){
@@ -27,6 +27,9 @@ class Dashboard extends Component {
           <div>
             <p className="lead text-muted">Welcome {user.name} </p>
             <p>You have not setup a profile, please add some information</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
           </div>
         );
 
