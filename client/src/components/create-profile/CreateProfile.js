782d77728417d6ac1bbfff97f79a8fc80e018ff5
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2019-08-27 09:09:57 -0600

    adding experience

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -124,12 +124,9 @@ class CreateProfile extends Component {
     // Select options for status
     const options = [
       { label: '* Select Professional Status', value: 0 },
-      { label: 'Developer', value: 'Developer' },
-      { label: 'Junior Developer', value: 'Junior Developer' },
-      { label: 'Senior Developer', value: 'Senior Developer' },
-      { label: 'Manager', value: 'Manager' },
-      { label: 'Student or Learning', value: 'Student or Learning' },
-      { label: 'Instructor or Teacher', value: 'Instructor or Teacher' },
+      { label: 'Artist', value: 'Artist' },
+      { label: 'Student', value: 'Student' },
+      { label: 'Architect', value: 'Architectr' },
       { label: 'Intern', value: 'Intern' },
       { label: 'Other', value: 'Other' }
     ];
