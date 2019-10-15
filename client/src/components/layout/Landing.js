830397aafea6ece5f200bd7a64bf95825d3e5c00
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2019-10-15 14:39:50 -0600

    adding latest changes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import {PropTypes} from 'prop-types';
 import {connect} from 'react-redux';
+//import ContactUs from './components/layout/ContactUs';
 
 class Landing extends Component {
 
@@ -22,7 +23,7 @@ class Landing extends Component {
                             Connecting Art Arround the World
                         </h1>
                         <p className="lead">
-                            Create a artist profile/portafolio, share what you are doing and get help from the community
+                            Crea un portafolio de proyectos, conecta con personas increibles y comparte tu experiencia.
                         </p>
                         <hr/>
                         <Link to="/register" 
@@ -33,12 +34,38 @@ class Landing extends Component {
                         className="btn btn-lg btn-light">
                         Login
                         </Link>
+                        <footer className="page-footer orange">
+                            <div className="container">
+                              <div className="row">
+                                <div className="col l6 s12">
+                                  <h5 className="text-white">Bio</h5>
+                                  <p className="text-white text-lighten-4">We are a team of entrepreneurs working on this project. Any feedback or support would help to continue the development on this project and is greatly appreciated.</p>
 
-                    </div>
+
+                                </div>
+                                <div className="col l3 s12">
+                                  <h5 className="text-white">Artcon</h5>
+                                  <ul>
+                                    <li><a className="text-white" href="#!">Sobre Nosotros</a></li>
+                                    <li><a className="text-white" href="#!">Contactanos</a></li>
+                                    <li><a className="text-white" href="#!">Soporte</a></li>
+                                    <li><a className="text-white" href="#!">Ayuda</a></li>
+                                  </ul>
+                                </div>
+                                <div className="col l3 s12" >
+                                  <h5 className="text-white">Connect</h5>
+                                  <ul>
+                                    <li><a className="text-white" align="left" href="#!"><i className="fab fa-facebook"/> Facebook</a></li>
+                                    <li><a className="text-white" align="left" href="#!"><i className="fab fa-instagram"/> Instagram</a></li>
+                                    <li><a className="text-white" align="left" href="#!"><i className="fab fa-twitter"/>Twitter</a></li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </div>
+                          </footer>
+                    </div>     
                 </div>
-
             </div>
-
         </div>
         
       </div>
