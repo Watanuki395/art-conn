aed8ca5219bf2633b860fb5d107aff030d63dfc6
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2019-10-18 17:35:40 -0600

    adding all code for deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-const upload = require('./routes/api/upload');
+
 
 const app = express();
 
@@ -36,7 +36,7 @@ require('./config/passport')(passport);
 app.use('/api/users',users);
 app.use('/api/profile',profile);
 app.use('/api/posts',posts);
-app.use('/api/upload',upload);
+
 
 // server static assets if in production
 
